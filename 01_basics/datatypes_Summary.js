4bd08aacd9da52ff852e89717561907b7c03524b
Nihar1604 <niharthalkar68@gmail.com>
2025-02-22 20:20:44 +0000

    summary datatypes

---
+++ 01_basics/datatypes_Summary.js
@@ -0,0 +1,25 @@
+
+// Primitive:- 7 categories:String,Number,Boolean,null,undefined,symbol,BigInt
+const score = 100 
+let scoreValue;
+
+const id =Symbol("123")
+const idOther = Symbol("123")
+console.log(id == idOther);
+
+// Refrence type:-Arrays ,Objects,Functions 
+
+// JS is a dynamically typed language
+const heros = ["Ironman" , "Spiderman" ,"shaktiman"];  //array
+
+let obj={
+    name:"Nihar",
+    age:21,
+}                     
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros
+);
