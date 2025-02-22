60150ef6d9871c1ee68159ffdc0d88185898d7cc
Nihar1604 <niharthalkar68@gmail.com>
2025-02-22 07:48:48 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all JS code as newer version
+
+alert(3+3)     //This can be used in browser console but not for nodejs
+
+let name ="nihar"  //string 
+let age = 18       // number -> 2 to power 53
+let isLoggeIn = true  //Boolean 
+
+/*
+Types of datatype:bigInt 
+                  boolean
+                  null-> Standalone value
+                  undefind->Value is not yet assigned
+                  symbol -> unique
+
+Object:-                  
+*/
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
+
