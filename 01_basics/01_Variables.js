def0e17ca477dd4669df081419bea6913acdaef3
Nihar1604 <niharthalkar68@gmail.com>
2025-02-22 07:49:27 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,16 @@
+console.log("Nihar")
+const accountId = 14553
+let accountEmail = "hitesh@gmail.com"
+var accountPass = "1234"
+accountCity =  "Jaipur"
+
+//accountId=2
+console.log(accountId)
+accountEmail="js@gmail.com"
+accountPass = "222222"
+accountCity = "Mumbai"
+
+//Declare var without any value
+let accountNo;
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountNo])   //To print data in tabular format
